ce5fccaaea81f82573f2dc2a894923b4e24846d9
Thibaux <thibaux.swildens@gmail.com>
2021-04-21 22:50:31 +0200

    Added next question button

--- src/components/quiz.vue
+++ src/components/quiz.vue
@@ -1,8 +1,33 @@
 <template>
   <main>
-    <h1>dit is de quuiz jonge!</h1>
+    <header>
+      <h3>Vraag {{ vraagID }}</h3>
+    </header>
 
-    <div class="endButton">
+    <section>
+      <div class="question">
+        <div class="artPiece">
+          <img src="" alt="" />
+          <div class="hints"></div>
+        </div>
+      </div>
+      <div class="multipleChoice">
+        <div class="choices">
+          <div class="choiceOne"></div>
+          <div class="choiceTwo"></div>
+          <div class="choiceThree"></div>
+          <div class="choiceFour"></div>
+        </div>
+      </div>
+    </section>
+
+    <div class="nextQuestionButton">
+      <button>
+        <div>Volgende vraag!</div>
+      </button>
+    </div>
+
+    <div v-if="endQuiz" class="endButton">
       <button
         data-hover="Let's not quiz!"
         @click="switchComponent('endQuiz')"
@@ -19,24 +44,95 @@ import { bus } from "./../main.js";
 
 export default {
   name: "quiz",
-
   props: {
     currentComp: {
       type: String,
       required: true,
     },
   },
+  data() {
+    return {
+      endQuiz: false,
+      vraagID: 1,
+    };
+  },
 
   methods: {
     switchComponent(comp) {
       bus.$emit("switchComp", comp);
+    },
+
+    endQuiz() {
+      this.endQuiz === true;
     },
   },
 };
 </script>
 
 <style scoped lang="scss">
-// Button magic
+// Next question button
+.nextQuestionButton {
+  margin: 10% auto;
+  text-align: center;
+
+  a:hover {
+    border-bottom: 1px solid #111;
+  }
+  p {
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    padding: 20px 0;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+  button {
+    background: transparent;
+    outline: none;
+    position: relative;
+    border: 2px solid #111;
+    padding: 15px 50px;
+    overflow: hidden;
+    // min-width: 10px;
+    // width: 100px;
+  }
+
+  button:hover {
+    background-color: #27375c;
+  }
+  button:before {
+    position: absolute;
+    top: 1.1em;
+    left: 0;
+    width: 100%;
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    font-weight: 800;
+    font-size: 0.8em;
+  }
+  button:hover div {
+    color: #ba9ea7;
+  }
+  button div {
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    font-weight: 800;
+    font-size: 110%;
+  }
+}
+
+.nextQuestionButton,
+.nextQuestionButton:before,
+.nextQuestionButton:after {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  text-decoration: none;
+  color: #111;
+}
+
+// End quiz button magic
 .endButton {
   min-width: 500px;
   margin: 10% auto;
